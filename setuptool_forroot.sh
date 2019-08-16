0423a5bd9d4507cc1526e4d6a3b28d1e8388be52
Lysithena <gotti0916@gmail.com>
2019-08-16 16:00:49 +0900

    fix

--- setuptool_forroot.sh
+++ setuptool_forroot.sh
@@ -1,11 +1,12 @@
-softwares="neovim python3-pip awesome git ibus-mozc"
+softwares="firefox vlc neovim python3-pip awesome git ibus-mozc"
 
+echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" >> /etc/apt/sources.list
 apt update
 apt upgrade
 apt -y install ${softwares}
+apt -y install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') nvidia-driver
 if [ ! -e "./Cica_v5.0.1_with_emoji.zip"];then
 wget https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
 fi
 unzip "./Cica_v5.0.1_with_emoji.zip"
 cp ./Cica-* /usr/local/share/fonts
-echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" >> /etc/apt/sources.list
