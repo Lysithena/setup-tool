81068161cc244ccaaa659d58abafbf23ceb98c4a
Lysithena <gotti0916@gmail.com>
2019-09-07 15:17:18 +0900

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,18 @@
-eval (hub alias -s)
+#eval (hub alias -s)
 alias b popd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias agi='sudo apt install'
+alias agr='sudo apt remove'
+alias agu='sudo apt update'
+alias top=htop
+alias rm='rm -i'
+alias pbcopy='xclip -selection c'
+alias pbpaste='xclip -selection c -o'
+
+alias vi=nvim
+alias vim=nvim
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \cx\ck peco_kill
